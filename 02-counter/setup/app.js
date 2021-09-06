5a7984571f84b5110216ad19bb8e513ac8ad1d76
Ujjwal-0 <kumarujjwal7349@gmail.com>
2021-09-07 00:33:22 +0530

    first commit

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,33 @@
+//set initil count
+let count = 0;
+
+//select values and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+console.log(btns);
+
+btns.forEach(function (btn){
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('increase')){
+            count++;
+        }
+        else{
+            count = 0;
+        }
+        if(count > 0){
+            value.style.color = "green";
+        }
+        if(count < 0){
+            value.style.color = "orange";
+        }
+        if(count == 0){
+            value.style.color = "black";
+        }
+        value.textContent = count;
+    })
+});
